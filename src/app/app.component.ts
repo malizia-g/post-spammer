67f2d51aa312b3cd2c6d51ea2f44493a8d15f78f
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-01-21 11:14:53 +0100

    progetto completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'post-spammer';
+  articles = new Array<string>();
+
+  addSpam(testo : HTMLInputElement , numero : HTMLInputElement) : boolean
+  {
+    let t : string = testo.value; //qui prendo il testo della casella di testo 1
+    let n : number = Number(numero.value); //qui prendo il numero nella casella di testo 2
+    
+    for (let index = 0; index < n; index++) {
+         this.articles.push(t);  //this serve a ottenere l'oggetto che sta eseguendo il codice corrente 
+         //push aggiunge un elemento al vettore  
+    }
+
+    //console.log(this.articles);
+    return false;   //ritornare false evita di far ricaricare la pagina al browser
+  }
+
 }
